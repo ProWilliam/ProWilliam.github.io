fe0f93ea6142eea395ae48cf59a9aa563d9674ea
William Torre <pro.williamtoro@gmail.com>
2024-04-04 15:03:30 -0500

    Added ctions git

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  server: { port: "https://ProWilliam.github.io"},
   integrations: [icon()],
   devToolbar: {
     enabled: false
